244c6eab57907a7f0b772a4e98864c3b2cc0a999
e <sevensfive7@163.com>
2018-07-15 12:57:38 +0800

    path

---
+++ Tree/124. Binary Tree Maximum Path Sum.java
@@ -0,0 +1,17 @@
+public class BinaryTreeMaximumPathSum {
+    int res;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) return 0;
+        res = Integer.MIN_VALUE;
+        helper(root);
+        return res;
+    }
+    public int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        res = Math.max(res, left + right + root.val);
+        return Math.max(left, right) + root.val;
+    }
+}
