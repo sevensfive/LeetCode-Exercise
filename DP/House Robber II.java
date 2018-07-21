b09294040a96d7261da44f42004efcb0dc2282f2
e <sevensfive7@163.com>
2018-07-21 21:01:05 +0800

    DP

---
+++ DP/House Robber II.java
@@ -0,0 +1,17 @@
+public class HouseRobberII {
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
+
+    public int rob(int[] nums, int lo, int hi) {
+        int prevNo = 0;
+        int prevYes = 0;
+        for (int i = lo; i <= hi; i++) {
+            int temp = prevNo;
+            prevNo = Math.max(prevNo, prevYes);
+            prevYes = nums[i] + temp;
+        }
+        return Math.max(prevNo, prevYes);
+    }
+}
